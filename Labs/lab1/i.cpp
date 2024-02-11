c00e37d67df167b5eaef3be76a20a27c9bfa79bc
Helder-PB <helder_apereira@hotmail.com>
2024-02-11 14:40:00 +0000

    I exercise made

--- Labs/lab1/i.cpp
+++ Labs/lab1/i.cpp
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*a program that concatenates all its arguments into a single string*/
+
 using namespace std;
 
 int main(int argc, char **argv){
@@ -13,5 +15,16 @@ int main(int argc, char **argv){
         counter++;
     }
     char* conct_str = (char*)malloc(sizeof(char)*result_len);
+	if(conct_str==NULL){printf("Allocation error");exit(0);}
+
+	int buff_ind = 0;
+	for(int i = 0; i < counter; i++){
+		strcpy(conct_str+buff_ind,argv[i]);
+		buff_ind+=strlen(argv[i]);
+	}
+	
+	printf("The concatenated string is :\n %s\n",conct_str);
+	free(conct_str);	
+
     return 0;
 }
